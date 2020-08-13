7f0200a082a81e2675b6b69a32ec523c7824475a
jsrav1 <rainerviguini1@hotmail.com>
2020-08-13 11:44:44 -0300

    update server

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
+
 const server = express()
 
-server.listen(5000, function(){
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function (req, res) {
+    return res.render("about")
+})
+
+server.get("/portfolio", function (req, res) {
+    return res.render("portfolio")
+})
+
+server.listen(5000, function () {
     console.log("server is running")
 })
